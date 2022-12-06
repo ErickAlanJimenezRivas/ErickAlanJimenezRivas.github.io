d2221d987dbc17337c8feb4e0aa087b2071e5d11
Erick Jimenez <massimo18x@hotmail.com>
2022-12-06 13:16:40 -0600

    Integración de menu mobile

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const navEmail = document.querySelector('.navbar-email');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const burguerMenu = document.querySelector('.menu');
+
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 
+burguerMenu.addEventListener('click', toogleBurguerMenu);
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleBurguerMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
