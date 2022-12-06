095a42c71cdbe5cd94671b59a0c07674410b2fdf
Erick Jimenez <massimo18x@hotmail.com>
2022-12-06 14:13:36 -0600

    Corrección de menu y carrito de compras

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const navEmail = document.querySelector('.navbar-email');
-
 const desktopMenu = document.querySelector('.desktop-menu');
-
 const burguerMenu = document.querySelector('.menu');
-
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shoping-cart');
+const productoCarrito = document.querySelector('.shoppingCart');
 
+menuCarritoIcon.addEventListener('click', togglecarritoCompras);
 navEmail.addEventListener('click', toggleDesktopMenu);
-
-burguerMenu.addEventListener('click', toogleBurguerMenu);
+burguerMenu.addEventListener('click', toggleBurguerMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    productoCarrito.classList.add('inactive');
 }
 
-function toogleBurguerMenu(){
+function toggleBurguerMenu(){
+    productoCarrito.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+function togglecarritoCompras(){
+    mobileMenu.classList.add('inactive');
+    productoCarrito.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
 }
